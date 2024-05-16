46b5876453e0593b830bfecc41d0af4eb4a8fba2
khaled98-j <khjabarin6@gmail.com>
2024-05-16 15:15:50 +0300

    Add project files.

---
+++ BookStore/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStore.Data;
+
+namespace BookStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+    }
+}
